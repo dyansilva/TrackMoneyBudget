f3d59280ca954d18b2aefbf1fe3905a748a59c29
Dyan <fascia_voile_0w@icloud.com>
2025-06-25 18:55:58 -0300

    Initial Commit

---
+++ TrackMoneyBudget/TrackMoneyBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackMoneyBudgetApp.swift
+//  TrackMoneyBudget
+//
+//  Created by Dyan silva on 25/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TrackMoneyBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
